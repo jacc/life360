6ae988291e32dc592ab0956e77bc729c9bd4c86e
Jack LaFond <jack.lafond@icloud.com>
2021-10-01 22:15:07 -0400

    feat: something 2

--- src/index.ts
+++ src/index.ts
@@ -16,25 +16,25 @@ export default class Life360 {
     });
   }
 
-  // TODO: return array of typed circle objects
-  public async getCircles(): Promise<[Life360Circle]> {
-    const response = await this.get(`circles`);
-    const data = await response.json();
-    return data as [Life360Circle];
-  }
-
-  // TODO: return typed circle object
-  public async getCircle(circleID: string): Promise<Life360Circle> {
-    const response = await this.get(`circles/${circleID}`);
-    const data = await response.json();
-    return data as Life360Circle;
-  }
+  // // TODO: return array of typed circle objects
+  // public async getCircles(): Promise<Life360Response> {
+  //   const response = await this.get(`circles`);
+  //   const data = await response.json();
+  //   return data;
+  // }
+  //
+  // // TODO: return typed circle object
+  // public async getCircle(circleID: string): Promise<Life360Circle> {
+  //   const response = await this.get(`circles/${circleID}`);
+  //   const data = await response.json();
+  //   return data as Life360Circle;
+  // }
 
   public async getUser(
     circleID: string,
     memberID: string
   ): Promise<Life360User> {
-    const response = await this.get(`circles/${circleID}/members/${member}`);
+    const response = await this.get(`circles/${circleID}/members/${memberID}`);
     const data = await response.json();
     return data as Life360User;
   }
